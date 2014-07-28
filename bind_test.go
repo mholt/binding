5fc63fb059fb89377078d4714abdd609917a6466
Billie H. Cleek <bhcleek@gmail.com>
2014-07-27 19:07:31 -0700

    add tests to verify form binding

--- bind_test.go
+++ bind_test.go
@@ -1,6 +1,8 @@
 package binding
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -50,5 +52,33 @@ func TestBind(t *testing.T) {
 		})
 
 	})
+}
 
+func TestBindForm(t *testing.T) {
+	Convey("Given a struct reference and complete form data", t, func() {
+		cm := NewCompleteModel()
+		formData := cm.FormValues()
+
+		Convey("Given that all of the struct's fields are required", func() {
+			model := AllTypes{}
+			Convey("When bindForm is called", func() {
+				req, err := http.NewRequest("POST", "http://www.example.com", nil)
+				So(err, ShouldBeNil)
+				var errs Errors
+				errs = bindForm(req, &model, formData, nil, errs)
+				Convey("Then all of the struct's fields should be populated", func() {
+
+				})
+
+				Convey("Then no errors should be produced", FailureContinues, func() {
+					So(errs.Len(), ShouldEqual, 0)
+					if errs.Len() > 0 {
+						for _, e := range errs {
+							Println(fmt.Sprintf("%v. %s", e.FieldNames, e.Message))
+						}
+					}
+				})
+			})
+		})
+	})
 }
--- validate_test.go
+++ validate_test.go
@@ -107,8 +107,150 @@ func (at *AllTypes) FieldMap() FieldMap {
 	}
 }
 
-func TestValidate(t *testing.T) {
+func (at *AllTypes) FormValues() map[string][]string {
+	fv := make(map[string][]string)
+	fm := at.FieldMap()
 
+	addField := func(key, value interface{}) {
+		f, ok := fm[key].(Field)
+		if ok {
+			if len(fv[f.Form]) == 0 {
+				fv[f.Form] = make([]string, 0, 1)
+			}
+			fv[f.Form] = append(fv[f.Form], fmt.Sprintf("%v", value))
+		}
+	}
+
+	addField(&at.Uint8, at.Uint8)
+	addField(&at.PointerToUint8, *at.PointerToUint8)
+	for _, v := range at.Uint8Slice {
+		addField(&at.Uint8Slice, v)
+	}
+	addField(&at.Uint16, at.Uint16)
+	addField(&at.PointerToUint16, *at.PointerToUint16)
+	for _, v := range at.Uint16Slice {
+		addField(&at.Uint16Slice, v)
+	}
+	addField(&at.Uint32, at.Uint32)
+	addField(&at.PointerToUint32, *at.PointerToUint32)
+	for _, v := range at.Uint32Slice {
+		addField(&at.Uint32Slice, v)
+	}
+	addField(&at.Uint64, at.Uint64)
+	addField(&at.PointerToUint64, *at.PointerToUint64)
+	for _, v := range at.Uint64Slice {
+		addField(&at.Uint64Slice, v)
+	}
+	addField(&at.Int8, at.Int8)
+	addField(&at.PointerToInt8, *at.PointerToInt8)
+	for _, v := range at.Int8Slice {
+		addField(&at.Int8Slice, v)
+	}
+	addField(&at.Int16, at.Int16)
+	addField(&at.PointerToInt16, *at.PointerToInt16)
+	for _, v := range at.Int16Slice {
+		addField(&at.Int16Slice, v)
+	}
+	addField(&at.Int32, at.Int32)
+	addField(&at.PointerToInt32, *at.PointerToInt32)
+	for _, v := range at.Int32Slice {
+		addField(&at.Int32Slice, v)
+	}
+	addField(&at.Int64, at.Int64)
+	addField(&at.PointerToInt64, *at.PointerToInt64)
+	for _, v := range at.Int64Slice {
+		addField(&at.Int64Slice, v)
+	}
+	addField(&at.Float32, at.Float32)
+	addField(&at.PointerToFloat32, *at.PointerToFloat32)
+	for _, v := range at.Float32Slice {
+		addField(&at.Float32Slice, v)
+	}
+	addField(&at.Float64, at.Float64)
+	addField(&at.PointerToFloat64, *at.PointerToFloat64)
+	for _, v := range at.Float64Slice {
+		addField(&at.Float64Slice, v)
+	}
+	addField(&at.Uint, at.Uint)
+	addField(&at.PointerToUint, *at.PointerToUint)
+	for _, v := range at.UintSlice {
+		addField(&at.UintSlice, v)
+	}
+	addField(&at.Int, at.Int)
+	addField(&at.PointerToInt, *at.PointerToInt)
+	for _, v := range at.IntSlice {
+		addField(&at.IntSlice, v)
+	}
+	addField(&at.Bool, at.Bool)
+	addField(&at.PointerToBool, *at.PointerToBool)
+	for _, v := range at.BoolSlice {
+		addField(&at.BoolSlice, v)
+	}
+	addField(&at.String, at.String)
+	addField(&at.PointerToString, *at.PointerToString)
+	for _, v := range at.StringSlice {
+		addField(&at.StringSlice, v)
+	}
+	addField(&at.Time, at.Time.Format(TimeFormat))
+	addField(&at.PointerToTime, *at.PointerToTime)
+	for _, v := range at.TimeSlice {
+		addField(&at.TimeSlice, v.Format(TimeFormat))
+	}
+
+	return fv
+}
+
+func NewCompleteModel() AllTypes {
+	model := AllTypes{}
+	model.Uint8 = 255
+	model.PointerToUint8 = &model.Uint8
+	model.Uint8Slice = []uint8{model.Uint8}
+	model.Uint16 = 65535
+	model.PointerToUint16 = &model.Uint16
+	model.Uint16Slice = []uint16{model.Uint16}
+	model.Uint32 = 4294967295
+	model.PointerToUint32 = &model.Uint32
+	model.Uint32Slice = []uint32{model.Uint32}
+	model.Uint64 = 18446744073709551615
+	model.PointerToUint64 = &model.Uint64
+	model.Uint64Slice = []uint64{model.Uint64}
+	model.Int8 = 127
+	model.PointerToInt8 = &model.Int8
+	model.Int8Slice = []int8{model.Int8}
+	model.Int16 = 32767
+	model.PointerToInt16 = &model.Int16
+	model.Int16Slice = []int16{model.Int16}
+	model.Int32 = 2147483647
+	model.PointerToInt32 = &model.Int32
+	model.Int32Slice = []int32{model.Int32}
+	model.Int64 = 9223372036854775807
+	model.PointerToInt64 = &model.Int64
+	model.Int64Slice = []int64{model.Int64}
+	model.Float32 = 3.14
+	model.PointerToFloat32 = &model.Float32
+	model.Float32Slice = []float32{model.Float32}
+	model.Float64 = 2.718
+	model.PointerToFloat64 = &model.Float64
+	model.Float64Slice = []float64{model.Float64}
+	model.Uint = 4294967295
+	model.PointerToUint = &model.Uint
+	model.UintSlice = []uint{model.Uint}
+	model.Int = 4294967295
+	model.PointerToInt = &model.Int
+	model.IntSlice = []int{model.Int}
+	model.Bool = true
+	model.PointerToBool = &model.Bool
+	model.BoolSlice = []bool{model.Bool}
+	model.String = "I'm a little teapot"
+	model.PointerToString = &model.String
+	model.StringSlice = []string{model.String}
+	model.Time = time.Now()
+	model.PointerToTime = &model.Time
+	model.TimeSlice = []time.Time{model.Time}
+
+	return model
+}
+func TestValidate(t *testing.T) {
 	Convey("Given a struct populated properly and as expected", t, func() {
 
 		Convey("No errors should be produced", FailureContinues, func() {
@@ -116,53 +258,7 @@ func TestValidate(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			model := AllTypes{}
-			model.Uint8 = 255
-			model.PointerToUint8 = &model.Uint8
-			model.Uint8Slice = []uint8{model.Uint8}
-			model.Uint16 = 65535
-			model.PointerToUint16 = &model.Uint16
-			model.Uint16Slice = []uint16{model.Uint16}
-			model.Uint32 = 4294967295
-			model.PointerToUint32 = &model.Uint32
-			model.Uint32Slice = []uint32{model.Uint32}
-			model.Uint64 = 18446744073709551615
-			model.PointerToUint64 = &model.Uint64
-			model.Uint64Slice = []uint64{model.Uint64}
-			model.Int8 = 127
-			model.PointerToInt8 = &model.Int8
-			model.Int8Slice = []int8{model.Int8}
-			model.Int16 = 32767
-			model.PointerToInt16 = &model.Int16
-			model.Int16Slice = []int16{model.Int16}
-			model.Int32 = 2147483647
-			model.PointerToInt32 = &model.Int32
-			model.Int32Slice = []int32{model.Int32}
-			model.Int64 = 9223372036854775807
-			model.PointerToInt64 = &model.Int64
-			model.Int64Slice = []int64{model.Int64}
-			model.Float32 = 3.14
-			model.PointerToFloat32 = &model.Float32
-			model.Float32Slice = []float32{model.Float32}
-			model.Float64 = 2.718
-			model.PointerToFloat64 = &model.Float64
-			model.Float64Slice = []float64{model.Float64}
-			model.Uint = 4294967295
-			model.PointerToUint = &model.Uint
-			model.UintSlice = []uint{model.Uint}
-			model.Int = 4294967295
-			model.PointerToInt = &model.Int
-			model.IntSlice = []int{model.Int}
-			model.Bool = true
-			model.PointerToBool = &model.Bool
-			model.BoolSlice = []bool{model.Bool}
-			model.String = "I'm a little teapot"
-			model.PointerToString = &model.String
-			model.StringSlice = []string{model.String}
-			model.Time = time.Now()
-			model.PointerToTime = &model.Time
-			model.TimeSlice = []time.Time{model.Time}
-
+			model := NewCompleteModel()
 			errs := Validate(req, &model)
 
 			expectedErrs := make(map[string]bool)
@@ -309,5 +405,4 @@ func TestValidate(t *testing.T) {
 		Convey("[]time.Time", nil)
 
 	})
-
 }
